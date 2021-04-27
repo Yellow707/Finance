e9c9430876dc216d09680b1f4faf4de3efa9e4b3
Igor Petrov <fox0234@yandex.ru>
2021-04-27 22:48:22 +0300

    implement AlfaProductsScrapper

--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,6 @@
 import config from './config.json';
 import { TinkoffProductsScrapper } from './BankDataScrapper/Tinkoff/TinkoffProductsScrapper';
+import { AlfaProductsScrapper } from './BankDataScrapper/Alfa/AlfaProductsScrapper';
 
 const url = config.tinkoff.baseUrl; // URL we're scraping
 const login = config.tinkoff.login;
@@ -9,8 +10,9 @@ const password = config.tinkoff.password;
     console.log(login);
     console.log(password);
 
-    var tinkoffScrapper = new TinkoffProductsScrapper(login, password, url)
-    var bankProducts = await tinkoffScrapper.bankProducts()
-    console.log(bankProducts)
-
+    var tinkoffScrapper = new TinkoffProductsScrapper(config.tinkoff.login, config.tinkoff.password, config.tinkoff.baseUrl)
+    var alfaScrapper = new AlfaProductsScrapper(config.alfa.login, config.alfa.password, config.alfa.baseUrl)
+    var banksProducts = await Promise.all([alfaScrapper.bankProducts(), tinkoffScrapper.bankProducts()])
+    var mergedProducts = banksProducts[0].concat(banksProducts[1])
+    console.log(mergedProducts)
   })()
